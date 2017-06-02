f064f8398f80dea3a96b032aba7ed2037f0e176a
Jefferson LE QUELLEC <jle-quel@e3r8p9.42.fr>
2017-06-02 10:59:38 +0200

    2017-06-02

---
+++ ft_ls.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ls.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jle-quel <jle-quel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/02 10:57:29 by jle-quel          #+#    #+#             */
+/*   Updated: 2017/06/02 10:59:28 by jle-quel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LS_H
+# define FT_LS_H
+
+# include "./libft/libft.h"
+# include <dirent.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <pwd.h>
+# include <uuid/uuid.h>
+# include <grp.h>
+# include <sys/xattr.h>
+# include <sys/xattr.h>
+# include <time.h>
+# include <stdio.h>
+# include <errno.h>
+# include <sys/xattr.h>
+
+
+
+#endif
